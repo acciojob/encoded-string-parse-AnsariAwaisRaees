00847abca3e1dddec39a99a59b2a5257fd90253a
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-11-28 21:58:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const parseCode = (str) => {
-  // your code here
+	// your code here
+	const reg = /^(.*?)000(.*?)(.*?)000(\d+)$/;
+	const match = str.match(reg);
+
+	if (match) {
+		const [, firstName, lastName, id] = match;
+		return {firstName, lastName, id};
+	}else{
+		return null;
+	}
 };
 
 // Do not change the code below
