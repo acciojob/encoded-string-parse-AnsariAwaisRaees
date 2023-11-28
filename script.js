66b7be0a76f2c9613f64da5038ed7b86c54fbf19
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-11-28 22:02:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const parseCode = (str) => {
 	// your code here
-	const reg = /^(.*?)000(.*?)(.*?)000(\d+)$/;
-	const match = str.match(reg);
+	const regex = /^(.*?)000(.*?)000(\d+)$/;
+  
+	const match = input.match(regex);
 
 	if (match) {
 		const [, firstName, lastName, id] = match;
-		return {firstName, lastName, id};
-	}else{
-		return null;
+	    return { firstName, lastName, id };
+	} else {
+	    return null;
 	}
 };
 
 // Do not change the code below
-const str = prompt("Enter str: ");
+// const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
